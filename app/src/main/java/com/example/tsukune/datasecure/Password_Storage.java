5c20f66421145a3700d22d205b667632b4da9309
Tsukune <yzy_71196@hotmail.com>
2018-06-08 03:44:51 +0800

    Current: -- add hash function for encryption key
    For Future:
    -- create and test database for:
       ->storing login details and encryption key
       ->password storage
    -- encryption/decryption for file storage

--- app/src/main/java/com/example/tsukune/datasecure/Password_Storage.java
+++ app/src/main/java/com/example/tsukune/datasecure/Password_Storage.java
@@ -1,38 +1,55 @@
 package com.example.tsukune.datasecure;
 
 import android.util.Base64;
-
+import org.mindrot.jbcrypt.BCrypt;
 import java.security.SecureRandom;
-
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Password_Storage {
 
+    private int ivSize = 16;
+
     //encryption key = password to unlock the storage
 
     public String Encrypt(String key, String password) throws Exception {
 
         //Generate random IV
-        byte[] ivBytes = new byte[16];
+        byte[] ivBytes = new byte[ivSize];
         SecureRandom rng = new SecureRandom();
         rng.nextBytes(ivBytes);
         IvParameterSpec iv = new IvParameterSpec(ivBytes);
 
-        //Generate Encryption Key
-        SecretKeySpec secKeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+        //Hash Encryption Key
+        String hashedKey = BCrypt.hashpw(key, BCrypt.gensalt(10));
+        SecretKeySpec secKeySpec = new SecretKeySpec(hashedKey.getBytes("UTF-8"), "AES");
 
         //Encrypt
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
         cipher.init(Cipher.ENCRYPT_MODE, secKeySpec, iv);
         byte[] encrypted = cipher.doFinal(password.getBytes());
-        return Base64.encodeToString(encrypted, Base64.DEFAULT);
+
+        //Combine IV with encrypted password
+        byte[] combined = new byte[ivSize + encrypted.length];
+        System.arraycopy(iv, 0, combined, 0, ivSize);
+        System.arraycopy(encrypted, 0, combined, ivSize, encrypted.length); //here ivSize start from 16 due to the array size is 0 to 15
+
+        return Base64.encodeToString(combined, Base64.DEFAULT);
     }
 
-    public String Decrypt(String key, String initVector, String EncryptedPassword) throws Exception {
+    public String Decrypt(String key, String EncryptedPassword) throws Exception {
 
-        IvParameterSpec iv = new IvParameterSpec(initVector.getBytes());
+        //Extract IV
+        byte[] ivBytes = new byte[ivSize];
+        System.arraycopy(EncryptedPassword, 0, ivBytes, 0, ivSize);
+        IvParameterSpec iv = new IvParameterSpec(ivBytes);
+
+        //Extract encrypted password
+        int encryptedSize = EncryptedPassword.length() - ivSize;
+        byte[] encryptedBytes = new byte[encryptedSize];
+        System.arraycopy(EncryptedPassword, ivSize, encryptedBytes, 0, encryptedSize);
+
         SecretKeySpec secKeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
 
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
--- app/src/main/java/com/example/tsukune/datasecure/Register_Password.java
+++ app/src/main/java/com/example/tsukune/datasecure/Register_Password.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-
 import java.util.regex.Pattern;
 
 public class Register_Password extends Fragment {
